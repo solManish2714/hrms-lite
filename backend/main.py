4eab8082a880e3825bf4a4829a4f7a3971aaea29
solManish2714 <solmanish2714@gmail.com>
2026-02-06 11:41:19 +0530

    Fix invalid CORS config (credentials with wildcard)

--- backend/main.py
+++ backend/main.py
@@ -10,12 +10,11 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI(title="HRMS Lite API")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # allow all origins
-    allow_credentials=True,
+    allow_origins=["*"],         
+    allow_credentials=False,      
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 def get_db():
     db = SessionLocal()
