1dab73d9860640ca2e20e303bb0b35cb88867651
solManish2714 <solmanish2714@gmail.com>
2026-02-06 11:19:06 +0530

    Allow Render frontend in CORS

--- backend/main.py
+++ backend/main.py
@@ -10,10 +10,15 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI(title="HRMS Lite API")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://hrms-frontend.onrender.com"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+)
+
 )
 
 
