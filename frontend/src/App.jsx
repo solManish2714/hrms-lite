3cc9e1bea7c6a51f263abfad9123ad5491c3d488
solManish2714 <solmanish2714@gmail.com>
2026-02-06 11:00:49 +0530

    Use Render backend URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://hrms-backend-f12a.onrender.com";
 
 function App() {
   const [employees, setEmployees] = useState([]);
